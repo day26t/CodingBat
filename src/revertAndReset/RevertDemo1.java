e86e93adb4f384a6cc6ac2c594b8457db90f8f64
Daiana <daiana@MacBook-Pro-Daiana.local>
2026-01-19 20:37:09 +0600

    Revert "name"
    This reverts commit 88b3026ea13cdb7c14d40496f930ce1e4963135c.

--- src/revertAndReset/RevertDemo1.java
+++ src/revertAndReset/RevertDemo1.java
@@ -3,6 +3,5 @@ package revertAndReset;
 public class RevertDemo1 {
     public static void main(String[] args) {
         System.out.println("First one");
-        System.out.println(My name is Daiana);
     }
 }
