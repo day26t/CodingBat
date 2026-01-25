47a76c32e7eef9e6266d77cd3dbc167ae6c5cc53
daiana <talasbaeva26d@gmail.com>
2026-01-25 13:38:59 +0600

    my name

--- src/resetAndRevert_11/RevertDemo11.java
+++ src/resetAndRevert_11/RevertDemo11.java
@@ -3,5 +3,6 @@ package resetAndRevert_11;
 public class RevertDemo11 {
     public static void main(String[] args) {
         System.out.println("First one");
+        System.out.println(My name is Dayana);
     }
 }
