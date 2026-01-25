4d8da766e2550c141b56426c5cfbc8f1effce7c9
daiana <talasbaeva26d@gmail.com>
2026-01-25 13:50:29 +0600

    Revert "my name"
    This reverts commit 47a76c32e7eef9e6266d77cd3dbc167ae6c5cc53.

--- src/resetAndRevert_11/RevertDemo11.java
+++ src/resetAndRevert_11/RevertDemo11.java
@@ -3,6 +3,5 @@ package resetAndRevert_11;
 public class RevertDemo11 {
     public static void main(String[] args) {
         System.out.println("First one");
-        System.out.println(My name is Dayana);
     }
 }
